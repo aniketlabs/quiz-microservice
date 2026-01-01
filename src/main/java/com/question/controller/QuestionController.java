b1bbcce0faddbb4efd9fd7f280d5b3d35e7336fd
Aniket Narayan Shinde <aniketnarayanshinde@Anikets-MacBook-Air.local>
2026-01-01 12:04:52 +0530

    Initial commit

---
+++ src/main/java/com/question/controller/QuestionController.java
@@ -0,0 +1,47 @@
+package com.question.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.question.entities.Question;
+import com.question.service.QuestionService;
+
+@RestController
+@RequestMapping("/question")
+public class QuestionController {
+	
+	private QuestionService questionService;
+
+	public QuestionController(QuestionService questionService) {
+		super();
+		this.questionService = questionService;
+	}
+	
+	
+	@PostMapping
+	public Question create(@RequestBody Question question) {
+		return questionService.create(question);
+	}
+	
+	@GetMapping
+	public List<Question> getAll(){
+		return questionService.get();
+	}
+	
+	@GetMapping("/{questionId}")
+	public Question get(@PathVariable  Long questionId) {
+		return questionService.getOne(questionId);
+	}
+	
+	@GetMapping("/quiz/{quizId}")
+	public List<Question> getQuestionOfQuiz(@PathVariable Long quizId) {
+		return questionService.getQuestionOfQuiz(quizId);
+	}
+
+}
